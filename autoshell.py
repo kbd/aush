0d4e3a981fa5351d03c68424594d6c067ca4f58f
Keith Devens <keith@keithdevens.com>
2021-05-30 23:43:06 -0600

    WIP: incomplete pass at piping
    last worked on a couple weeks ago, saving where I was

--- autoshell.py
+++ autoshell.py
@@ -10,34 +10,69 @@
 # with a non-zero error code
 
 import logging
-import subprocess
-from collections import namedtuple
+from subprocess import PIPE, Popen
 
 log = logging.getLogger(__name__)
 
-Result = namedtuple("Result", ['code', 'stdout', 'stderr'])
 
-class ShellCommand:
-    _default_kwargs = {'check': True, 'capture_output': True, 'text': True}
+class Command:
+    _default_kwargs = {'stdout': PIPE, 'stderr': PIPE, 'text': True}
     _command: list[str]
     def __init__(self, *args, **kwargs):
-        self._command = list(args)
+        self._command = [*args]
         self._kwargs = self._default_kwargs | kwargs
 
     def __call__(self, *args, **kwargs):
         # interpreting of types can be done, like if you pass an un-called
-        # ShellCommand instead of a str you call it, but for now take strs
-        cmd = self._command + list(args)
-        runargs = self._kwargs | kwargs
-        log.warning(f"Executing: {cmd} with arguments {runargs}")
-        result = subprocess.run(cmd, **runargs)
-        if result.returncode:
-            log.error(f"Return code {result.returncode} from process")
-
-        return Result(result.returncode, result.stdout, result.stderr)
+        # Command instead of a str you call it, but for now take strs
+        if args or kwargs:
+            return Command(*(self._command + [*args]), **(self._kwargs | kwargs))
+        else:
+            log.warning(f"Executing: {self._command} with arguments {self._kwargs}")
+            result = Popen(self._command, **self._kwargs)
+            return Result(result)
 
     def __getattr__(self, name):
-        return ShellCommand(*(self._command + [name]), **self._kwargs)
+        return Command(*(self._command + [name]), **self._kwargs)
+
+    def __str__(self):
+        return str(self._command)
+
+
+class Result:
+    def __init__(self, process):
+        self._process = process
+        self._waited = False
+
+    def wait(self):
+        if self._waited:
+            return
+
+        if self._process.stdin and self._process.stdin != PIPE:
+            # https://docs.python.org/3.9/library/subprocess.html#replacing-shell-pipeline
+            self._process.stdin.close()
+
+        self.stdout, self.stderr = self._process.communicate()
+        self.code = self._process.returncode
+        self.waited = True
+
+    def __getattr__(self, name):
+        if not self._waited:
+            if name in ['returncode', 'stdout', 'stderr']:
+                self.wait()
+                return getattr(self, name)
+
+        return getattr(self._process, name)
+
+    def __or__(self, other):
+        """Pipe two Commands together
+
+        This directly connects the stdout left -> right like a shell pipeline
+        so that commands run in parallel
+        """
+        stdin = {'input': self.stdout} if self._waited else {'stdin': self._process.stdout}
+        return Command(*other._command, **(other._kwargs | stdin))
+
 
 if __name__ == '__main__':
     logging.basicConfig()
